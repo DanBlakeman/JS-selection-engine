a4bbaa42a8512c9d20c9026e37ac47bdb05449ba
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-06-11 13:15:33 +0100

    WIP can find dom element using tag or id

--- Answer.js
+++ Answer.js
@@ -1,31 +1,53 @@
 var $ = function (selector) {
   var elements = [];
 
-  var findHtmlTag = selector.match(/[a-z]*/)[0];
+
+
+  var findHtmlTag = function() {
+   if (selector.match(/[a-z]*/)[0]) {
+      return selector.match(/[a-z]*/)[0]
+      ;
+    }
+    return null;
+  };
 
   var findClass = function() {
     if (selector.match(/(\.\w*)/)) {
-      console.log(selector.match(/(\.\w*)/)[0]);
-    } else {
-      console.log('none');
+      return selector.match(/(\.\w*)/)[0];
     }
+    return null;
   };
 
   var findId = function() {
-    if (selector.match(/(\#\w*)/)) {
-      console.log(selector.match(/(\#\w*)/)[0]);
-    } else {
-      console.log('none');
+    if (selector.match(/#(\w+)/)) {
+      return selector.match(/#(\w+)/)[1];
     }
+    return null;
+  };
+
+  var toBeSelected;
+
+  if (findHtmlTag()) {
+    toBeSelected = document.body.getElementsByTagName(findHtmlTag());
+  } else {
+    toBeSelected = document.body.getElementsByTagName('*');
+  }
+
+  if (findId()) {
+    var newToBeSelected = [];
+    for (var i = 0; i < toBeSelected.length; i++) {
+      console.log(toBeSelected[i].id);
+      console.log(findId());
+      if (toBeSelected[i].id === findId()) {
+        newToBeSelected.push(toBeSelected[i])
+      };
+    }
+    toBeSelected = newToBeSelected;
   };
 
 
 
-  // if (findClass) {
-  //   return document.body.getElementsByTagName(findTag);
-  // }
 
-
-
-  return elements;
+  console.log(toBeSelected);
+  return toBeSelected;
 };
